cd1233a37ccd5ed4fd3641c215ed352184fc8ef3
Michael Pavlov <micha.pavlov@gmail.com>
2023-07-08 12:13:21 +0300

    Add level goals

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField] 
+    private string levelToLoad;
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.gameObject.CompareTag("Player")) return;
+
+        // TODO: add a transition
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
